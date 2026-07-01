8b1edaae023f8280c2737ba7f5a8f23ef6f841c7
Elyashiv <elyashiv988@gmail.com>
2026-07-01 17:52:44 +0300

    added fun main

--- grade_helper/main.py
+++ grade_helper/main.py
@@ -1,59 +1,61 @@
 
-students = [
-    ("Dana", 82),
-    ("Tom", 55),
-    ("Maya", 91),
-    ("Ron", 60),
-    ("Noa", "88"),
-    ("Ben", 120),
-    ("Lior", -5),
-    (55, 70),
-    ["bob",5]]
+students = [("Dana", 82),("Tom", 55),("Maya", 91),("Ron", 60),("Noa", "88"),("Ben", 120),("Lior", -5),(55, 70),["bob",5]]
 
-valid_list=[]
-def validation_data(all,name,grade):
-    
-    
-    if type(all)==tuple and type(name)==str and type(grade)==int:
-        
-        valid_list.append(all)
+
+def validation_type(data):
+    if type(data)==tuple:
+        return True
     else:
-        valid_list.append("invalid value")
+        print("invalid data")
 
+def validation_name(name):            
+    if type(name)==str:
+        return True
+    else:
+        print("invalid name")    
 
-
-
-
+def validation_grade(grade):
+    if type(grade)==int and 0<grade<101:
+        return True
+    else:
+        print("invalid grade")
+               
+            
 
 def print_status(name,grade):
     
+    if grade >=90:
+       print(name,grade,"excellent!")
+    elif 60<=grade<90:
+        print(name,grade,"passed!")
+    else:
+        print(name,grade,"failed!")
     
-        if grade >=90:
-            print(name,grade,"excellent!")
-        elif 60<=grade<90:
-            print(name,grade,"passed!")
-        else:
-            print(name,grade,"failed!")
-
-
-
-def calc_average(averege):
+    return grade
     
-    for i in valid_list:
-        averege+=i[1]
-    print(f"the averege is {averege/len(valid_list)}")
+  
+
+def calc_average(grade_list):
+    
+    try:
+        print(f"the averege is {sum(grade_list)/len(grade_list)}")
+    except ZeroDivisionError:
+        print("there isn't any grades")
 
 
 def main():
-    while True:
-        for i in students:
-            validation_data(i,i[0],i[1])
-        for i in valid_list:
-            print_status(i[0],i[1])
-        break
-    calc_average(0)
+    
+    passed_grades=[] 
+    for i in students:
+        
+        if validation_type(i):
+            if validation_name(i[0]):
+                if validation_grade(i[1]):  
+                    grade=print_status(i[0],i[1])
+                    passed_grades.append(grade)
 
-main()
+                    
+    calc_average(passed_grades)
 
-
+main()      
 
